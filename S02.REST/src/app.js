b3ebd792851577f0c5471f29cda16d884af77bcd
AnthonyGrenier <2071623@cstj.qc.ca>
2024-08-30 14:16:41 -0400

    dab

--- S02.REST/src/app.js
+++ S02.REST/src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 const app = express();
 
+
+//Route: /Status
 app.get('/status',(req,res)=>{
     res.status(200).end();
 });
@@ -11,4 +13,13 @@ app.get('/', (req,res)=>{
     res.send('<h1>Première route test </h1>');
 });
 
+//Route : /math/somme
+app.get('/math/somme', (req, res) => {
+    //endpoint : /math/somme
+    const a =8;
+    const b =7;
+    res.status(200);
+    res.set('content-type', 'text/plain');
+    res.send(`somme: ${a+b}`);
+});
 export default app;
